ae3d59dec79bd7682aebbd970e95d7ec2373213f
jt00721 <titusgloverj@gmail.com>
2025-03-17 20:21:35 +0000

    Remove error logging from repository layer

--- internal/repository/habitRepository.go
+++ internal/repository/habitRepository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"log"
-
 	"github.com/jt00721/habit-tracker/internal/domain"
 	"gorm.io/gorm"
 )
@@ -12,52 +10,33 @@ type HabitRepository struct {
 }
 
 func (repo *HabitRepository) Create(habit *domain.Habit) error {
-	if err := repo.DB.Create(habit).Error; err != nil {
-		log.Println("Error creating habit via repo:", err)
-		return err
-	}
-	return nil
+	return repo.DB.Create(habit).Error
 }
 
 func (repo *HabitRepository) GetByID(id uint) (*domain.Habit, error) {
 	var habit domain.Habit
-	if err := repo.DB.First(&habit, id).Error; err != nil {
-		log.Println("Error getting habit by ID via repo:", err)
-		return nil, err
-	}
-	return &habit, nil
+	err := repo.DB.First(&habit, id).Error
+	return &habit, err
 }
 
 func (repo *HabitRepository) GetAll() ([]domain.Habit, error) {
 	var habits []domain.Habit
-	if err := repo.DB.Find(&habits).Error; err != nil {
-		log.Println("Error getting all habits via repo:", err)
-		return nil, err
-	}
-	return habits, nil
+	err := repo.DB.Find(&habits).Error
+	return habits, err
 }
 
 func (repo *HabitRepository) Update(habit *domain.Habit) error {
-	if err := repo.DB.Save(habit).Error; err != nil {
-		log.Println("Error updating habit via repo:", err)
-		return err
-	}
-	return nil
+	return repo.DB.Save(habit).Error
 }
 
 func (repo *HabitRepository) Delete(id uint) error {
-	if err := repo.DB.Delete(&domain.Habit{}, id).Error; err != nil {
-		log.Println("Error deleting habit via repo:", err)
-		return err
-	}
-	return nil
+	return repo.DB.Delete(&domain.Habit{}, id).Error
 }
 
 func (repo *HabitRepository) SafeUpdate(habit *domain.Habit) error {
 	tx := repo.DB.Begin()
 	if err := tx.Save(habit).Error; err != nil {
 		tx.Rollback()
-		log.Println("Error updating habit transaction:", err)
 		return err
 	}
 	return tx.Commit().Error
@@ -66,9 +45,5 @@ func (repo *HabitRepository) SafeUpdate(habit *domain.Habit) error {
 func (repo *HabitRepository) GetStreaks() ([]domain.Habit, error) {
 	var habits []domain.Habit
 	err := repo.DB.Where("current_streak > ?", 0).Order("current_streak DESC").Find(&habits).Error
-	if err != nil {
-		log.Println("Error fetching habit streaks:", err)
-		return nil, err
-	}
-	return habits, nil
+	return habits, err
 }
