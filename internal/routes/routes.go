51c1d5c1faa5995acc84d26e666397214473af3d
jt00721 <titusgloverj@gmail.com>
2025-03-19 08:47:39 +0000

    Define API endpoints using Gin framework

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jt00721/habit-tracker/internal/handler"
+	"github.com/jt00721/habit-tracker/internal/usecase"
+)
+
+func SetupRoutes(router *gin.Engine, uc *usecase.HabitUsecase) {
+	habitHandler := &handler.HabitHandler{Usecase: uc}
+
+	router.POST("/api/habits", habitHandler.CreateHabitApi)
+	router.GET("/api/habits", habitHandler.GetAllHabitsApi)
+	router.GET("/api/habits/:id", habitHandler.GetHabitByIDApi)
+	router.PUT("/api/habits/:id", habitHandler.UpdateHabitApi)
+	router.DELETE("/api/habits/:id", habitHandler.DeleteHabitApi)
+	router.GET("/api/habits/with_streaks", habitHandler.GetStreaksApi)
+	router.PUT("/api/habits/:id/mark_complete", habitHandler.MarkHabitCompletedApi)
+}
