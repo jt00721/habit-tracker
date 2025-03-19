d5a1cf7212522a92d0023f408bfe20892c5296b2
jt00721 <titusgloverj@gmail.com>
2025-03-19 08:53:58 +0000

    Add NewApp() function and run application in entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/jt00721/habit-tracker/config"
+)
+
+func main() {
+	// Initialize application
+	application := config.NewApp()
+	// Run application
+	application.Run()
+}
